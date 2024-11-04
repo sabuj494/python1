4912ca6136ace0a04efe7dbe63bc4d3dcf581a0c
Sabuj Das <112347578+sabuj494@users.noreply.github.com>
2024-11-04 12:11:43 +0600

    Add files via upload

---
+++ finalproject.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name genarator")
+street =input("what is the name of the city you grew up in?\n")
+pet=input("what is the name of your pet?\n")
+print("your band name "+street   +" " + pet)
